78a6571f885686354b5f3430301995fb6b673edb
MCT32 <ivanhen99@gmail.com>
2024-12-01 15:15:56 +1100

    fixed checks

--- main.py
+++ main.py
@@ -4,29 +4,37 @@ from flask import Flask, request
 import time
 
 THRESHOLD = 255
+HOST = "192.168.0.136"
 
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 async def hello_world():
-    if request.json["player"]["state"]["flashed"]:
-        if request.json["previously"]["player"]["state"]["flashed"]:
+    if not "player" in request.json:
+        return ""
+    if not "state" in request.json["player"]:
+        return ""
+
+    if "previously" in request.json:
+        if "state" in request.json["previously"]:
             if request.json["player"]["state"]["flashed"] >= THRESHOLD and request.json["previously"]["player"]["state"]["flashed"] < THRESHOLD:
-                dev = await kasa.Discover.discover_single("192.168.0.136")
+                dev = await kasa.Discover.discover_single(HOST)
                 await dev.turn_on()
                 await dev.update()
+                return ""
             if request.json["player"]["state"]["flashed"] < THRESHOLD and request.json["previously"]["player"]["state"]["flashed"] >= THRESHOLD:
-                dev = await kasa.Discover.discover_single("192.168.0.136")
+                dev = await kasa.Discover.discover_single(HOST)
                 await dev.turn_off()
                 await dev.update()
-        else:
-            if request.json["player"]["state"]["flashed"] >= THRESHOLD:
-                dev = await kasa.Discover.discover_single("192.168.0.136")
-                await dev.turn_on()
-                await dev.update()
-            else:
-                dev = await kasa.Discover.discover_single("192.168.0.136")
-                await dev.turn_off()
-                await dev.update()
+                return ""
+    
+    if request.json["player"]["state"]["flashed"] >= THRESHOLD:
+        dev = await kasa.Discover.discover_single(HOST)
+        await dev.turn_on()
+        await dev.update()
+    else:
+        dev = await kasa.Discover.discover_single(HOST)
+        await dev.turn_off()
+        await dev.update()
 
     return ""
