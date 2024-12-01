fb43d87954593ea7e3104c7e1a2a30c6292b7df2
MCT32 <ivanhen99@gmail.com>
2024-12-01 15:50:04 +1100

    lots of errors but just ignore them

--- main.py
+++ main.py
@@ -3,38 +3,47 @@ import kasa
 from flask import Flask, request
 import time
 
-THRESHOLD = 255
+THRESHOLD = 127
 HOST = "192.168.0.136"
 
-app = Flask(__name__)
+DEVICE = None
 
-@app.route("/", methods=['POST'])
-async def hello_world():
-    if not "player" in request.json:
-        return ""
-    if not "state" in request.json["player"]:
-        return ""
+async def async_setup():
+    print("SETUPSETUPSETUPSETUPSETUPSETUPSETUPSETUPSETUPSETUPSETUP")
+    global DEVICE
+    DEVICE = await kasa.Discover.discover_single(HOST)
 
-    if "previously" in request.json:
-        if "state" in request.json["previously"]:
-            if request.json["player"]["state"]["flashed"] >= THRESHOLD and request.json["previously"]["player"]["state"]["flashed"] < THRESHOLD:
-                dev = await kasa.Discover.discover_single(HOST)
-                await dev.turn_on()
-                await dev.update()
-                return ""
-            if request.json["player"]["state"]["flashed"] < THRESHOLD and request.json["previously"]["player"]["state"]["flashed"] >= THRESHOLD:
-                dev = await kasa.Discover.discover_single(HOST)
-                await dev.turn_off()
-                await dev.update()
-                return ""
+def create_app():
+    app = Flask(__name__)
+
+    with app.app_context():
+        asyncio.run(async_setup())
+
+    @app.route("/", methods=['POST'])
+    async def hello_world():
+        if not "player" in request.json:
+            return ""
+        if not "state" in request.json["player"]:
+            return ""
+
+        if "previously" in request.json:
+            if "state" in request.json["previously"]:
+                if request.json["player"]["state"]["flashed"] >= THRESHOLD and request.json["previously"]["player"]["state"]["flashed"] < THRESHOLD:
+                    await DEVICE.turn_on()
+                    await DEVICE.update()
+                    return ""
+                if request.json["player"]["state"]["flashed"] < THRESHOLD and request.json["previously"]["player"]["state"]["flashed"] >= THRESHOLD:
+                    await DEVICE.turn_off()
+                    await DEVICE.update()
+                    return ""
+        
+        if request.json["player"]["state"]["flashed"] >= THRESHOLD:
+            await DEVICE.turn_on()
+            await DEVICE.update()
+        else:
+            await DEVICE.turn_off()
+            await DEVICE.update()
+
+        return ""
     
-    if request.json["player"]["state"]["flashed"] >= THRESHOLD:
-        dev = await kasa.Discover.discover_single(HOST)
-        await dev.turn_on()
-        await dev.update()
-    else:
-        dev = await kasa.Discover.discover_single(HOST)
-        await dev.turn_off()
-        await dev.update()
-
-    return ""
+    return app
