ceeda9c9e694eda0e7e0bf7b3fc9333ca8aef3ea
MCT32 <ivanhen99@gmail.com>
2024-12-01 15:08:20 +1100

    added adjustable threshold and extra cases to prevent sending unnecessary requests

--- main.py
+++ main.py
@@ -3,18 +3,30 @@ import kasa
 from flask import Flask, request
 import time
 
+THRESHOLD = 255
+
 app = Flask(__name__)
 
 @app.route("/", methods=['POST'])
 async def hello_world():
     if request.json["player"]["state"]["flashed"]:
-        if request.json["player"]["state"]["flashed"] >= 255:
-            dev = await kasa.Discover.discover_single("192.168.0.136")
-            await dev.turn_on()
-            await dev.update()
+        if request.json["previously"]["player"]["state"]["flashed"]:
+            if request.json["player"]["state"]["flashed"] >= THRESHOLD and request.json["previously"]["player"]["state"]["flashed"] < THRESHOLD:
+                dev = await kasa.Discover.discover_single("192.168.0.136")
+                await dev.turn_on()
+                await dev.update()
+            if request.json["player"]["state"]["flashed"] < THRESHOLD and request.json["previously"]["player"]["state"]["flashed"] >= THRESHOLD:
+                dev = await kasa.Discover.discover_single("192.168.0.136")
+                await dev.turn_off()
+                await dev.update()
         else:
-            dev = await kasa.Discover.discover_single("192.168.0.136")
-            await dev.turn_off()
-            await dev.update()
+            if request.json["player"]["state"]["flashed"] >= THRESHOLD:
+                dev = await kasa.Discover.discover_single("192.168.0.136")
+                await dev.turn_on()
+                await dev.update()
+            else:
+                dev = await kasa.Discover.discover_single("192.168.0.136")
+                await dev.turn_off()
+                await dev.update()
 
     return ""
