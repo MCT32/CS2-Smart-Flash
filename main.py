2042405bddb9af8503ef1ffc5c63cb3ac1a3fbb8
MCT32 <ivanhen99@gmail.com>
2024-12-01 14:51:20 +1100

    controlling light from flask

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 import asyncio
 import kasa
+from flask import Flask
 
-async def main():
+from flask import Flask
+import time
+
+app = Flask(__name__)
+
+@app.route("/")
+async def hello_world():
     dev = await kasa.Discover.discover_single("192.168.0.136")
+    await dev.turn_on()
+    await dev.update()
+
+    time.sleep(0.5)
+
     await dev.turn_off()
     await dev.update()
 
-if __name__ == "__main__":
-    asyncio.run(main())
+    return ""
