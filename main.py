6306b0c5725df4180ca0c712dd4e1ef93a5f314b
MCT32 <ivanhen99@gmail.com>
2024-12-01 15:04:52 +1100

    gamestate integration working

--- main.py
+++ main.py
@@ -1,21 +1,20 @@
 import asyncio
 import kasa
-from flask import Flask
-
-from flask import Flask
+from flask import Flask, request
 import time
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST'])
 async def hello_world():
-    dev = await kasa.Discover.discover_single("192.168.0.136")
-    await dev.turn_on()
-    await dev.update()
-
-    time.sleep(0.5)
-
-    await dev.turn_off()
-    await dev.update()
+    if request.json["player"]["state"]["flashed"]:
+        if request.json["player"]["state"]["flashed"] >= 255:
+            dev = await kasa.Discover.discover_single("192.168.0.136")
+            await dev.turn_on()
+            await dev.update()
+        else:
+            dev = await kasa.Discover.discover_single("192.168.0.136")
+            await dev.turn_off()
+            await dev.update()
 
     return ""
